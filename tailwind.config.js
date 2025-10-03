0544610a226885dda0a57df21ea3d4279179b148
Matt Beckstrand <matt.beckstrand@gmail.com>
2025-10-03 11:49:50 -0600

    started styling index page

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          brand: {
+            DEFAULT: 'white',
+            light: 'rgb(48, 46, 46)',
+            dark: 'rgb(24, 23, 23)',
+        
+      },
+    },
+  },
+},
+    plugins: [],
+  }
+  
