1a19de40215d176c38d92da12a8bd4201ea4d760
Matt Beckstrand <matt.beckstrand@gmail.com>
2025-10-23 15:17:53 -0600

    reactifying profile page

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import { useEffect } from "react";
+import React, { useState } from 'react';
+
 
 export function Profile() {
+
+  const [username, setUsername] = useState('loading...');
+  const [displayName, setDisplayName] = useState('loading...');
+  const [reviews, setReviews] = useState([])
+
+
+  useEffect(() => {
+    setUsername('mattbeckstrand');
+    setDisplayName('Matt Beckstrand');
+    setReviews([{id: 1, title:'Swag', artist:'Justin Bieber', album: 'Swag', rating: 4, artworkLocation: '/Images/Swag.png', text:"I thought this album was good but I didn't think it was amazing. Justin Bieber had 3 good songs in this."},
+      {id: 2, title:'Swag II', artist:'Justin Bieber', album: 'Swag II', rating: 2, artworkLocation: '/Images/Swag.png', text:"Has some good parts but ultimately not as good as Swag I"
+      }
+    ])
+  }, [])
+
+  function renderStars(rating) {
+    const filled = '★'.repeat(rating);
+    const unFilled = '☆'.repeat(5 - rating);
+    return filled + unFilled;
+  }
+
   return (
     <main className="p-4">
       <div className="flex items-center space-x-6 mb-6">
         <img src="/Images/IMG_2769.jpg" alt="Profile" className="w-20 h-20 rounded-full" />
         <div>
-          <h2 className="text-2xl font-bold">Matt Beckstrand</h2>
-          <p className="text-gray-300">matt.beckstrand</p>
+          <h2 className="text-2xl font-bold">{displayName}</h2>
+          <p className="text-gray-300">{username}</p>
         </div>
         <div className="flex space-x-6 ml-auto">
           <span>Followers 10</span>
@@ -17,15 +40,19 @@ export function Profile() {
       </div>
       <hr />
       <div className="mt-4">
-        <div className="flex items-center space-x-2 mb-3">
+        {reviews.map((review) =>
+        (<div key={review.id}>
+          <div className="flex items-center space-x-2 mb-3">
           <img src="/Images/ProfileIcon.webp" alt="Profile" width="26" height="26" className="rounded-full" />
-          <span className="font-semibold">Matt Beckstrand</span>
+          <span className="font-semibold">{username}</span>
         </div>
-        <img src="/Images/Swag.png" alt="Swag - Justin Bieber" width="200" className="rounded-lg mb-2" />
-        <p className="font-semibold">Swag - Justin Bieber</p>
-        <p className="text-yellow-400">★ ★ ★ ☆ ☆</p>
-        <p className="text-gray-300">I thought this album was good but I didn't think it was amazing. Justin Bieber had 3 good songs in this.</p>
-      </div>
+        <img src={review.artworkLocation} alt={review.title + '-'  + review.artist} width="200" className="rounded-lg mb-2" />
+        <p className="font-semibold">{review.title}</p>
+        <p className="text-yellow-400">{renderStars(review.rating)}</p>
+        <p className="text-gray-300">{review.text}</p>
+        </div>
+        ))}
+        </div>
     </main>
   );
 }
